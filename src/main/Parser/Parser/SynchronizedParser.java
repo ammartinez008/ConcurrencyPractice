e5babbfd01b701e4c37574e03182ad7219911470
alx <ammartinez008@hotmail.com>
2016-05-17 01:44:59 -0400

    reworked synchronizedParser with subclass executor

--- src/main/Parser/Parser/SynchronizedParser.java
+++ src/main/Parser/Parser/SynchronizedParser.java
@@ -1,5 +1,6 @@
 package main.Parser.Parser;
 
+import main.Parser.ThreadPool.ParserThreadPool;
 import main.Parser.Util.SynchronizedWorker;
 import main.Parser.Util.TextGrabber;
 
@@ -11,18 +12,23 @@ import java.util.concurrent.*;
  * Created by alx on 4/30/16.
  */
 public class SynchronizedParser implements Parser {
+    private static final long MAXTIME = 10;
+
     private HashMap<String, Integer> wordMap;
     private List<String> lines;
     private TextGrabber textGrabber;
-    private static final int NTHREADS = 8;
-    private ExecutorService executorService
-            = Executors.newFixedThreadPool(NTHREADS);
+    private static final int NTHREADS = Runtime.getRuntime().availableProcessors();
+    private ExecutorService executorService;
+    private BlockingQueue<Runnable> queue;
+
     private HashMap<SynchronizedWorker, Future<?>> workers;
 
     public SynchronizedParser() {
         this.textGrabber = new TextGrabber();
         this.wordMap = new HashMap<>();
         this.workers = new HashMap<>();
+        this.queue = new LinkedBlockingQueue<>();
+        this.executorService = new ParserThreadPool(NTHREADS, NTHREADS + 1, MAXTIME, TimeUnit.SECONDS, queue);
     }
 
 
